2c45cb7c70cf370916b78dada0333912b4f97bf4
MadhavKrishna09 <103254884+MadhavKrishna09@users.noreply.github.com>
2024-01-10 18:58:49 +0530

    Time: 5 ms (86.66%) | Memory: 42.4 MB (26.86%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -1,27 +1,49 @@
+// class Solution {
+//     public String getHint(String secret, String guess) {
+//         int bulls = 0, cows = 0;
+//         StringBuilder st = new StringBuilder(secret);
+//         StringBuilder gs = new StringBuilder(guess);
+
+//         // Count bulls and remove matched characters
+//         for (int i = 0; i < secret.length(); i++) {
+//             if (secret.charAt(i) == guess.charAt(i)) {
+//                 bulls++;
+//                 st.setCharAt(i, '*'); // Mark characters to ignore later
+//                 gs.setCharAt(i, '*');
+//             }
+//         }
+
+//         // Count cows
+//         for (int i = 0; i < st.length(); i++) {
+//             if (st.charAt(i) != '*' && gs.indexOf(String.valueOf(st.charAt(i))) != -1) {
+//                 cows++;
+//                 int index = gs.indexOf(String.valueOf(st.charAt(i)));
+//                 gs.setCharAt(index, '*'); // Mark characters to ignore later
+//             }
+//         }
+
+//         return bulls + "A" + cows + "B";
+//     }
+// }
 class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0, cows = 0;
-        StringBuilder st = new StringBuilder(secret);
-        StringBuilder gs = new StringBuilder(guess);
+        int[] count = new int[10]; // To count occurrences of digits
 
-        // Count bulls and remove matched characters
         for (int i = 0; i < secret.length(); i++) {
-            if (secret.charAt(i) == guess.charAt(i)) {
-                bulls++;
-                st.setCharAt(i, '*'); // Mark characters to ignore later
-                gs.setCharAt(i, '*');
-            }
-        }
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
 
-        // Count cows
-        for (int i = 0; i < st.length(); i++) {
-            if (st.charAt(i) != '*' && gs.indexOf(String.valueOf(st.charAt(i))) != -1) {
-                cows++;
-                int index = gs.indexOf(String.valueOf(st.charAt(i)));
-                gs.setCharAt(index, '*'); // Mark characters to ignore later
+            if (s == g) {
+                bulls++; // Count bulls in the same position
+            } else {
+                // Count cows for digits not bulls
+                if (count[s - '0']++ < 0) cows++; // Increment for secret digit, check if already seen in guess
+                if (count[g - '0']-- > 0) cows++; // Decrement for guess digit, check if already seen in secret
             }
         }
 
         return bulls + "A" + cows + "B";
     }
 }
+
