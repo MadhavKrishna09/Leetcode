afc372058469eb41b52f4f9f126767b268783072
MadhavKrishna09 <103254884+MadhavKrishna09@users.noreply.github.com>
2024-03-05 17:39:15 +0530

    Time: 3 ms (100.00%) | Memory: 45.5 MB (20.32%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,22 +1,33 @@
 class Solution {
     public int minimumLength(String s) {
-        int i=0;
-        int j=s.length()-1;
+        // int i=0;
+        // int j=s.length()-1;
 
-        while(i<j){
-            if(s.charAt(i) == s.charAt(j)){
-                while(s.charAt(i) == s.charAt(i+1) & i+1<j){
-                    i++;
-                }
-                i++;
-                while(s.charAt(j) == s.charAt(j-1) & j-1>i){
-                    j--;
-                }
-                j--;
+        // while(i<j){
+        //     if(s.charAt(i) == s.charAt(j)){
+        //         while(s.charAt(i) == s.charAt(i+1) & i+1<j){
+        //             i++;
+        //         }
+        //         i++;
+        //         while(s.charAt(j) == s.charAt(j-1) & j-1>i){
+        //             j--;
+        //         }
+        //         j--;
                 
-            }
-            else break;
+        //     }
+        //     else break;
+        // }
+        //  return j-i+1;
+        char[] ch = s.toCharArray();
+        int n = ch.length;
+        int i=0;
+        int j=n-1;
+        while(j>i){
+            if(ch[i]!=ch[j]) break;
+            char c = ch[i];
+            while(j>=i && ch[i]==c)i++;
+            while(j>=i && ch[j]==c)j--;
         }
-         return j-i+1;
+        return j-i+1;
     }
 }
