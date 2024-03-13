2e3f766c557a1e9ac17343ded3f6dc22f3a6fc47
MadhavKrishna09 <103254884+MadhavKrishna09@users.noreply.github.com>
2024-03-13 18:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (88.87%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,22 +1,31 @@
-class Solution {
-    public int sumOfElements(int f,int l){
-        int sum =0;
-        for(int i=f;i<=l;i++){
-            sum += i;
-        }
-        return sum;
-    }
+// class Solution {
+//     public int sumOfElements(int f,int l){
+//         int sum =0;
+//         for(int i=f;i<=l;i++){
+//             sum += i;
+//         }
+//         return sum;
+//     }
 
 
-    public int pivotInteger(int n) {
-        int pivot = 1;
-        while(pivot<=n){
-            if(sumOfElements(1,pivot) == sumOfElements(pivot,n))
-                return pivot;
-            else pivot++;
-        }
+//     public int pivotInteger(int n) {
+//         int pivot = 1;
+//         while(pivot<=n){
+//             if(sumOfElements(1,pivot) == sumOfElements(pivot,n))
+//                 return pivot;
+//             else pivot++;
+//         }
 
-        return -1;
+//         return -1;
         
+//     }
+// }
+
+class Solution {
+    public int pivotInteger(int n) {
+        int y = n*(n+1)/2;
+        int x = (int)Math.sqrt(y);
+        if(x*x==y) return x;
+        else return -1;
     }
 }
