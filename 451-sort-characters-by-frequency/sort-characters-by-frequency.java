501bd7dc3c6ee96df2f7c62f42957f83c77bce48
MadhavKrishna09 <103254884+MadhavKrishna09@users.noreply.github.com>
2024-05-31 01:37:41 +0530

    Time: 15 ms (56.67%) | Memory: 45.2 MB (77.81%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -12,15 +12,28 @@ class Solution {
             mp.put(c, mp.getOrDefault(c,0) +1);
         }
 
-        List<Map.Entry<Character,Integer>> ls = new ArrayList<>(mp.entrySet());
-        ls.sort(Map.Entry.comparingByValue());
+        PriorityQueue<Character> minHeap = new PriorityQueue<>((a,b) -> mp.get(b) - mp.get(a));
+
+        // List<Map.Entry<Character,Integer>> ls = new ArrayList<>(mp.entrySet());
+        // ls.sort(Map.Entry.comparingByValue());
         String ans = "";
-        for(Map.Entry<Character, Integer> m:ls){
-            int n = m.getValue();
-            String s1 = String.valueOf(m.getKey());
-            ans = ans + s1.repeat(n);
+        // for(Map.Entry<Character, Integer> m:ls){
+        //     int n = m.getValue();
+        //     String s1 = String.valueOf(m.getKey());
+        //     ans = ans + s1.repeat(n);
+        // }
+
+        for(char c : mp.keySet()){
+            minHeap.offer(c);
+            
         }
 
-        return new StringBuilder(ans).reverse().toString();
+        while(minHeap.size()>0){
+            int freq = mp.get(minHeap.peek());
+            char ch = (char) minHeap.poll();
+            ans += String.valueOf(ch).repeat(freq);
+        }
+
+        return ans;
     }
 }
