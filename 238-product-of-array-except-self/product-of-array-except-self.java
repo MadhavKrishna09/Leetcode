63d33a063b91cb922071bca284d9b2a52e620d3c
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2024-08-02 18:34:19 +0530

    Time: 2 ms (86.36%) | Memory: 55.2 MB (63.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,19 +3,23 @@ class Solution {
         int prefixSum = 0;
         int n =nums.length;
         int[] ans = new int[n];
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        prefix[0] = 1;
-        suffix[n-1] = 1;
+        // int[] prefix = new int[n];
+        // // int[] suffix = new int[n];
+        // prefix[0] = 1;
+        // suffix[n-1] = 1;
+        int  suffix =1;
+        ans[0]=1;
 
 
         for(int i=1;i<nums.length;i++){
-            prefix[i] = nums[i-1]*prefix[i-1];
-            suffix[n-i-1] = nums[n-i]*suffix[n-i];  
+            // prefix[i] = nums[i-1]*prefix[i-1];
+            // suffix[n-i-1] = nums[n-i]*suffix[n-i];    
+            ans[i] = ans[i-1]*nums[i-1];
         }
 
-        for(int i =0;i<n;i++){
-            ans[i] = prefix[i]*suffix[i];
+        for(int i =n-1;i>=0;i--){
+            ans[i] = suffix * ans[i];
+            suffix = suffix * nums[i];
         }
         return ans;
         
