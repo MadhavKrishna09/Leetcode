1a523d3ec3195de090d0995b7b5958e99040bbea
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-03-24 12:09:46 +0530

    Time: 4 ms (50.45%) | Memory: 48.7 MB (98.14%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,87 +1,34 @@
-public class pair {
-    int first;
-    int second;
-    pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-// WE can direct store array also in queue by writing Queue<int[]> q = new....
 class Solution {
-    public void bfs(int i, int j,int[][] visited, char[][] grid){
-        Queue<pair> q = new LinkedList<>();
-        visited[i][j] = 1;
-        q.add(new pair(i,j));
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        while(!q.isEmpty()){
-            int row = q.peek().first;
-            int col = q.peek().second;
-            q.remove();
-            
-
-            for (int[] dir : directions) {
-                int nrow = row + dir[0];
-                int ncol = col + dir[1];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-                grid[nrow][ncol] == '1' && visited[nrow][ncol]==0){
-                    q.add(new pair(nrow, ncol));
-                    visited[nrow][ncol] = 1;
-                    }
-            }
-
-            // for(int delrow =-1; delrow<2;delrow++){
-            // for(int delcol =-1;delcol<2;delcol++){
-            //     int nrow = row+delrow;
-            //     int ncol = col + delcol;
-            //     if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
-            //     grid[nrow][ncol] == '1' && visited[nrow][ncol]==0){
-            //         q.add(new pair(nrow, ncol));
-            //         visited[nrow][ncol] = 1;
-            //     }
-            // }
-
-        }
-
-        }
-
-
-    public void dfs(int r, int c, char[][] grid){
-        grid[r][c] = '0';
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
-        for(int[] dir : directions){
-            int nrow = r+dir[0];
-            int ncol = c+dir[1];
-            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && grid[nrow][ncol] == '1'){
-                dfs(nrow,ncol,grid);
-            }
-        }
-
-
+    public void dfs(char[][] grid, int i, int j, int[][] visited, int r, int c){
+        if(grid[i][j] == '0' ) return;
+        // visited[i][j] = 1;
+        if(grid[i][j] == '1' && visited[i][j] == 0) {
+            visited[i][j] =1;
+            // ans++;
+            if(i-1>=0) dfs(grid, i-1, j, visited, r, c);
+            if(i+1<r)  dfs(grid, i+1, j, visited, r, c);
+            if(j+1<c)  dfs(grid, i, j+1, visited, r, c);
+            if(j-1>=0) dfs(grid, i, j-1, visited, r, c);
+         }
+        else return;
+        
     }
 
     public int numIslands(char[][] grid) {
-        
-        int count =0;
         int r = grid.length;
         int c = grid[0].length;
         int[][] visited = new int[r][c];
         int ans = 0;
+
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] == '1' && visited[i][j] == 0){
+                if(grid[i][j] != '0'  && visited[i][j] == 0) {
                     ans++;
-                    dfs(i,j,grid);
-
+                    dfs(grid, i, j, visited, r, c);
                 }
             }
         }
 
         return ans;
-        
     }
 }
