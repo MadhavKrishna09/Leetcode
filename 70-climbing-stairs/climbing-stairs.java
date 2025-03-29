25d8244cf28947078662eb328a83c5e9165663f6
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-03-30 01:33:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.98%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,35 @@
 class Solution {
     public int climbStairs(int n) {
-        // int[] dp = new int[n+1];
-        if(n<=1) return 1;
-        // if(n<0) return 0;
+        // // int[] dp = new int[n+1];
+        // if(n<=1) return 1;
+        // // if(n<0) return 0;
 
-        // if(dp[n]!= 0) return dp[n];
-        // dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        // // if(dp[n]!= 0) return dp[n];
+        // // dp[n] = climbStairs(n-1) + climbStairs(n-2);
 
-        int prev1 = 1;
-        int prev2 = 1;
-        int curr;
+        // int prev1 = 1;
+        // int prev2 = 1;
+        // int curr;
 
-        for(int i=2;i<=n;i++){
-            curr= prev1+prev2;
-            prev1=prev2;
-            prev2 = curr;
+        // for(int i=2;i<=n;i++){
+        //     curr= prev1+prev2;
+        //     prev1=prev2;
+        //     prev2 = curr;
             
-        }
+        // }
 
-        return prev2;
+        // return prev2;
+        int[] dp = new int[n+1];
+        return helper(dp, n);
+
+
+    }
+
+    public int helper(int[] dp, int n){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = helper(dp, n-1) + helper(dp, n-2);
 
     }
 }
