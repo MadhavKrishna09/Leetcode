fa25b6bcbee52aae5fbcea8ab1714d89bed842b5
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-04-12 12:23:35 +0530

    Time: 5 ms (87.63%) | Memory: 44.8 MB (55.74%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,25 +1,71 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // Set<Character> st = new HashSet<>();
-        Map<Character, Integer> mp = new HashMap<>();
-        int windowStart = 0;
         int ans = 0;
-        if(s.isEmpty()) return 0;
-        // if(s == null || s.trim().isEmpty()) return 1;
-
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(mp.containsKey(c) && mp.get(c) >= windowStart){
-                // ans = Math.max(ans, i-windowStart);
-                windowStart = mp.get(c)+1;
-
-                // windowStart++;
+        int i = 0;
+        int j = 0;
+        int n = s.length();
+        // HashSet<Integer> st = new HashSet<>();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        // for(int i = 0;i<s.length();i++){
+        //     // HashSet<Integer> 
+        //     if(st.)
+        // }
+        while(j<n){
+            if(mp.containsKey(s.charAt(j))) {
+                i = Math.max(i, mp.get(s.charAt(j)) + 1);
             }
-            mp.put(c, i); 
-            ans = Math.max(ans, i - windowStart + 1);
+            mp.put(s.charAt(j), j);
+            ans = Math.max(j-i +1, ans);
+            j++;
         }
 
         return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Set<Character> st = new HashSet<>();
+        // Map<Character, Integer> mp = new HashMap<>();
+        // int windowStart = 0;
+        // int ans = 0;
+        // if(s.isEmpty()) return 0;
+        // // if(s == null || s.trim().isEmpty()) return 1;
+
+        // for(int i=0;i<s.length();i++){
+        //     char c = s.charAt(i);
+        //     if(mp.containsKey(c) && mp.get(c) >= windowStart){
+        //         // ans = Math.max(ans, i-windowStart);
+        //         windowStart = mp.get(c)+1;
+
+        //         // windowStart++;
+        //     }
+        //     mp.put(c, i); 
+        //     ans = Math.max(ans, i - windowStart + 1);
+        // }
+
+        // return ans;
 
 
         
