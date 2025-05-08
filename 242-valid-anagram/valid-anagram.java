515d44e069cfa84ff5ac5e5cd3cd595bfdaa49e0
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-05-08 13:43:39 +0530

    Time: 13 ms (32.05%) | Memory: 44.8 MB (35.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if(s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
         Map<Character, Integer> mp = new HashMap<>();
-        // We need frequency not just char.
-        for(char c : s.toCharArray()){
-            mp.put(c, mp.getOrDefault(c, 0)+1);
+        for(char c: s.toCharArray()){
+            mp.put(c, mp.getOrDefault(c, 0) +1);
+
+        }
+        for(char c: t.toCharArray()){
+            if(!mp.containsKey(c)) return false;
+            int n = mp.get(c);
+            n--;
+            mp.put(c,n);
+        }
+        for(char c: s.toCharArray()){
+            if(mp.get(c) !=  0) return false;
         }
 
-        for(char c : t.toCharArray()){
-            mp.put(c, mp.getOrDefault(c, 0) - 1);
-        }
+        return true;
 
-        for(int val : mp.values()){
-            if(val !=0) return false;
-        }
-    return true;
     }
 }
