c1f85661e8003da7b53c8596f9e1f54952730a71
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-05-16 18:29:09 +0530

    Time: 4 ms (90.16%) | Memory: 44.8 MB (55.55%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,89 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-    //     int ans = 0;
-    //     int i = 0;
-    //     int j = 0;
-    //     int n = s.length();
-    //     // HashSet<Integer> st = new HashSet<>();
-    //     HashMap<Character, Integer> mp = new HashMap<>();
-    //     // for(int i = 0;i<s.length();i++){
-    //     //     // HashSet<Integer> 
-    //     //     if(st.)
-    //     // }
-    //     while(j<n){
-    //         if(mp.containsKey(s.charAt(j))) {
-    //             i = Math.max(i, mp.get(s.charAt(j)) + 1);
-    //         }
-    //         mp.put(s.charAt(j), j);
-    //         ans = Math.max(j-i +1, ans);
-    //         j++;
-    //     }
+        int n = s.length();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        if(n<=1) return n;
+        int i = 0;
+        int j=0;
+        int ans =0;
+        while(j<n){
+            if(mp.containsKey(s.charAt(j))){
+                i = Math.max(i, mp.get(s.charAt(j)) + 1);
+                // j++;
+            }
+            
+            mp.put(s.charAt(j), j);
+            j++;
+            ans=Math.max(ans, j-i);
 
-    //     return ans;
-    int n = s.length();
-    int maxLen = 0;
-    int left = 0, right = 0;
-    HashSet<Character> set = new HashSet<>();
-
-    while(right < n) {
-        if (!set.contains(s.charAt(right))) {
-            set.add(s.charAt(right));
-            maxLen = Math.max(maxLen, right - left + 1);
-            right++;
-        } else {
-            set.remove(s.charAt(left));
-            left++;
+            
+            
+            // j++;
         }
-    }
-    return maxLen;
+
+        return ans;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Set<Character> st = new HashSet<>();
-        // Map<Character, Integer> mp = new HashMap<>();
-        // int windowStart = 0;
-        // int ans = 0;
-        // if(s.isEmpty()) return 0;
-        // // if(s == null || s.trim().isEmpty()) return 1;
-
-        // for(int i=0;i<s.length();i++){
-        //     char c = s.charAt(i);
-        //     if(mp.containsKey(c) && mp.get(c) >= windowStart){
-        //         // ans = Math.max(ans, i-windowStart);
-        //         windowStart = mp.get(c)+1;
-
-        //         // windowStart++;
-        //     }
-        //     mp.put(c, i); 
-        //     ans = Math.max(ans, i - windowStart + 1);
-        // }
-
-        // return ans;
-
-
-        
     }
 }
