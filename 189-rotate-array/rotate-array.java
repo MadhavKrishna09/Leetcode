473e40b2a4484ad25ffa31480d16208124899e6a
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-05-16 15:05:18 +0530

    Time: 2 ms (28.02%) | Memory: 57.5 MB (51.17%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,22 +1,39 @@
 class Solution {
+    public void reverse(int[] nums, int i, int j){
+        while(i<=j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;j--;
+        }
+    }
+
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
-        Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new LinkedList<>();
         if(n==1) return;
         if(k==0) return;
-        for(int i:nums){
-            q.offer(i);
-        }
-        int j = k;
+        // for(int i:nums){
+        //     q.offer(i);
+        // }
+        // int j = k;
 
-        for(int i = 0;i<j;i++){
-            nums[i] = nums[Math.abs(n-k)];
-            k--;
-        }
-        for(int i = j;i<n;i++){
-            nums[i] = q.poll();
-        }
+        // for(int i = 0;i<j;i++){
+        //     nums[i] = nums[(n-k)];
+        //     k--;
+        // }
+        // for(int i = j;i<n;i++){
+        //     nums[i] = q.poll();
+        // }
+
+        int i = 0;
+        int j= n-1;
+        reverse(nums, i, j);
+        reverse(nums, i, k-1);
+        reverse(nums, k, j);
+
+
 
 
         // if(n%2==0){
@@ -45,4 +62,6 @@ class Solution {
         // }
         // }
     }
+
+
 }
