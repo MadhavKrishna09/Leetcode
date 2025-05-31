d7ea8a43730c08f037d60409f2c8f13b6fdf7370
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-05-31 09:22:33 +0530

    Time: 35 ms (30.32%) | Memory: 53.2 MB (47.33%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,24 +1,28 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    public int[][] kClosest(int[][] points, int K) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
-            double distA = Math.sqrt(a[0] * a[0] + a[1] * a[1]);
-            double distB = Math.sqrt(b[0] * b[0] + b[1] * b[1]);
-            return Double.compare(distB, distA); // Max-heap based on distance
-        });
-
-        for (int[] point : points) {
-            maxHeap.offer(point);
-            if (maxHeap.size() > K) {
-                maxHeap.poll(); // Remove the farthest point if size exceeds K
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<double[]> pq = new PriorityQueue<>((double[] a,double[] b) -> Double.compare(b[2] , a[2]));
+        int n = points.length;
+        for(int[] i : points){
+            int x = i[0];
+            int y = i[1];
+            double d = Math.sqrt(x*x + y*y);
+            if(pq.size() < k){
+                pq.offer(new double[]{x,y,d});
+                
+            }
+            else if(d < (pq.peek()[2])){
+                pq.poll();
+                pq.offer(new double[]{x,y,d});
             }
         }
-
-        int[][] result = new int[K][2];
-        for (int i = K - 1; i >= 0; i--) {
-            result[i] = maxHeap.poll(); // Retrieve points in reverse order for correct answer
+        int[][] ans = new int[k][2];
+        int i = 0;
+        while(pq.size()>0 && i<k){
+            ans[i][0] = (int)pq.peek()[0];
+            ans[i][1] = (int)pq.peek()[1];
+            i++;
+            pq.poll();
         }
-        return result;
+        return ans;
     }
 }
