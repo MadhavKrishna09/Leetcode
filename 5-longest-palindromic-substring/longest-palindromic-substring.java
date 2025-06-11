3d43f29c3ea3120c6415d48c51fc6286663c8020
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-06-11 11:33:23 +0530

    Time: 19 ms (54.83%) | Memory: 45.3 MB (34.73%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,158 +1,39 @@
 class Solution {
-    // public static String longestPalindrome(String a){
-    //     int n = a.length();
-    //     String ans = "";
-    //     for(int i = 0;i<n;i++){
-    //         // StringBuilder sb = new StringBuilder();
-    //         String temp = expand(a, i, n);
-    //         if(temp.length() > ans.length()){
-    //             ans = temp;
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-
-    // public static String expand(String a, int i, int n){
-    //     StringBuilder sb = new StringBuilder();
-    //     if(i==0) return a.subString()
-    //     while(i>=0 && i<n){
-    //         int j = 1;
-    //         sb.append(a.charAt(i));
-    //         if(a.charAt(i-j) == a.charAt(i+j)){
-    //             sb.append(a.charAt(i-1));
-    //             j++;
-    //         }
-    //         else break;
-    //     }
-
-    //     return sb.toString();
-    // }
-
-    // public static String longestPalindrome(String a){
-    //     int n = a.length();
-    //     int[][] dp = new int[n+1][n+1];
-    //     for(int i = 1;i<=n;i++){
-    //         for(int j = 1;j<=n;j++){
-    //             dp[i][j] = -1;
-    //             // dp[0][i] = -1;
-    //         }
-    //     }
-    //     StringBuilder sb = new StringBuilder(a);
-    //     int m = sb.length();
-    //     String b = sb.reverse().toString();
-    //     for(int i = 1;i<=n;i++){
-    //         for(int j= 1;j<=m;j++){
-    //             if(a.charAt(i-1) == b.charAt(j-1)){
-    //                 dp[i][j] = 1+dp[i-1][j-1];
-    //             }
-    //             else {
-    //                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-    //             }
-    //         }
-    //     }
-
-        
-
-    //     // int ans = LCS(a, b, n, m, dp);
-    //     StringBuilder str = new StringBuilder();
-    //     while(n>0 && m>0){
-    //         if(a.charAt(n-1) == b.charAt(m-1)){
-    //             str.append(a.charAt(n-1));
-    //             n--;
-    //             m--;
-    //         }
-    //         else{
-    //             if(dp[n-1][m] >= dp[n][m-1]){
-    //                 n--;
-    //             }
-    //             else if(dp[n][m-1] > dp[n-1][m]){
-    //                 m--;
-    //             }
-    //         }
-    //     }
-
-    //     return str.toString();
-    // }
-// }
-
-
-    // public static int LCS(String a, String b,int n,int m, int[][] dp){
-    //     if(n==0 || m==0) return 0;
-    //     if(dp[n][m] != -1) return dp[n][m];
-
-    //     if(a.charAt(n-1) == b.charAt(m-1)){
-    //         return dp[n][m] = 1 + LCS(a, b, n-1, m-1, dp);
-    //     }
-    //     else{
-    //         return dp[n][m] = Math.max(LCS(a, b, n-1, m, dp), LCS(a, b, n, m-1, dp));
-    //     }
-    // }
-// }
-    
-
-
-
-    // public String longestPalindrome(String s) {
-    //     String ans = "";
-    //     int len = 0;
-
-    //     for(int i = 0;i<s.length();i++){
-    //         int l = i;
-    //         int r = i;
-           
-    //         while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r)){
-    //             if(r-l+1 > len){
-    //                 ans = s.substring(l,r+1);
-    //                 len = r-1+1;
-    //             }
-    //             l--;r++;
-    //         }
-            
-            
-    //         l = i;
-    //         r = i+1;
-    //         while(l>=0 && r<s.length() && s.charAt(l) == s.charAt(r)){
-    //             if(r-l+1 > len){
-    //                 ans = s.substring(l,r+1);
-    //                 len = r-1+1;
-    //             }
-    //             l--;r++;
-    //         }
-            
-
-    //     }
-
-    //     return ans;
-    // }
-
+    int max = 0;
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
+        // StringBuilder sb  = new StringBuilder();
+        String ans = "";
+        int n = s.length();
+        // int max 
+        if(n== 1) return s;
+        for(int i = 0;i<n;i++){
+            // StringBuilder sb = new StringBuilder();
+
+            String a = checkPalindrome(s, i, i);
+            String b = checkPalindrome(s, i, i+1);
+            if(a.length()>ans.length()) {
+                ans = a;
+            }
+            if(b.length() > ans.length()){
+                ans = b;
+            }
         }
 
-        String maxStr = s.substring(0, 1);
+        return ans;
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            String odd = expandFromCenter(s, i, i);
-            String even = expandFromCenter(s, i, i + 1);
-
-            if (odd.length() > maxStr.length()) {
-                maxStr = odd;
-            }
-            if (even.length() > maxStr.length()) {
-                maxStr = even;
-            }
-        }
-
-        return maxStr;
     }
 
-    private String expandFromCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private String checkPalindrome(String s, int i, int j){
+        int n = s.length();
+        while(i>=0 && j<n){
+            if(s.charAt(i) == s.charAt(j)){
+                // sb.add(s.charAt(i));
+                i--;
+                j++;
+            }
+            else{ break; }
         }
-        return s.substring(left + 1, right);
+
+        return  s.substring(i + 1, j);
     }
 }
