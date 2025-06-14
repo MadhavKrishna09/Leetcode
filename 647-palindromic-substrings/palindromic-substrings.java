abd4b7aea92bb4b04ee7cc9d10c62d90fed16447
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-06-14 09:35:56 +0530

    Time: 17 ms (32.23%) | Memory: 45.5 MB (16.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,24 +1,29 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
-        int odd=0, even = 0;
+        int count = 0;
+        boolean[][] dp = new boolean[n][n];
+        for(int i=0;i<n;i++){
+            dp[i][i] = true;
+            count++;
 
-        for(int i = 0;i<n;i++){
-            int l  =i;
-            int r = i;
-            odd += count(s,l,r);
-            even += count(s,l,r+1);
         }
 
-        return odd+even;
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                count++;
+                dp[i][i+1] = true;
+            }
+        }
 
-    }
-
-    public int count(String s , int l, int r){
-        int count = 0;
-        while(l>=0&&r<s.length()&&s.charAt(l) == s.charAt(r)){
-            count++;
-            l--;r++;
+        for(int len=3;len<=n;len++){
+            for(int i = 0;i<=n-len;i++){
+                int j = i+len-1;
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                    count++;
+                    dp[i][j] = true;
+                }
+            }
         }
 
         return count;
