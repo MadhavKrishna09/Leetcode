636ee1d9267d5800e03e31ae0cdc8256a853e260
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-07-09 23:47:14 +0530

    Time: 42 ms (86.27%) | Memory: 44.7 MB (51.74%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,22 +1,37 @@
+// class Solution {
+//     public int triangularSum(int[] nums) {
+//         int n  = nums.length;
+//         if(n==1) {
+//             return nums[0];
+//         }
+//        List<Integer> arr = new ArrayList<>();
+//         for (int num : nums) {
+//             arr.add(num);
+//         }
+//         while(arr.size()>1){
+//             for(int i = 0;i<arr.size()-1;i++){
+//                 int f = arr.get(i);
+//                 int s = arr.get(i+1);
+//                 arr.set(i, (f+s)%10);
+//             }
+//             arr.remove(arr.size()-1);
+//         }
+
+//         return arr.get(0);
+//     }
+// }
 class Solution {
     public int triangularSum(int[] nums) {
-        int n  = nums.length;
-        if(n==1) {
-            return nums[0];
+        int n=nums.length;
+        int k=n;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<k-1;j++)
+            {
+            nums[j]=(nums[j]+nums[j+1])%10;
         }
-       List<Integer> arr = new ArrayList<>();
-        for (int num : nums) {
-            arr.add(num);
+          k--;
         }
-        while(arr.size()>1){
-            for(int i = 0;i<arr.size()-1;i++){
-                int f = arr.get(i);
-                int s = arr.get(i+1);
-                arr.set(i, (f+s)%10);
-            }
-            arr.remove(arr.size()-1);
-        }
-
-        return arr.get(0);
+        return nums[0];
     }
 }
