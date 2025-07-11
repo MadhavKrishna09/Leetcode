ff41c76aa69ef01488a469e0ffb6dc4ae5b2469f
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-07-11 16:43:56 +0530

    Time: 6 ms (98.25%) | Memory: 48.1 MB (35.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,45 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // can we short a word based on its asscii value of its letters.
-        // Map<String, List<String>> anagramGroups = new HashMap<>();
-        
-        // // Iterate through each word in the input array
-        // for (String word : strs) {
-        //     // Sort the characters in the word to create a key
-        //     char[] charArray = word.toCharArray();
-        //     Arrays.sort(charArray);
-        //     String key = new String(charArray);
-            
-        //     // If key is not in the map, add a new entry with the word as a list
-        //     if (!anagramGroups.containsKey(key)) {
-        //         anagramGroups.put(key, new ArrayList<>());
-        //         anagramGroups.get(key).add(word);
-        //     }
-        //     // If key is already present, append the word to the existing list
-        //     else {
-        //         anagramGroups.get(key).add(word);
-        //     }
-        // }
-        
-        // // Convert map values to a list and return
-        // return new ArrayList<>(anagramGroups.values());
-
+        List<List<String>> answer = new ArrayList<>();
         Map<String, List<String>> mp = new HashMap<>();
-        for(String word: strs){
-            char[] arr = word.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
 
-            if(!mp.containsKey(key)){
-                mp.put(key, new ArrayList<>());
-                mp.get(key).add(word);
+        for(String str: strs){
+            char[] temp = str.toCharArray();
+            // String temp = Arrays.sort(str.toCharArray());
+            Arrays.sort(temp);
+            if(mp.containsKey(String.valueOf(temp))){
+                mp.get(String.valueOf(temp)).add(str);
             }
             else{
-                mp.get(key).add(word);
+                List<String> t = new ArrayList<>();
+                t.add(str);
+                mp.put(String.valueOf(temp) , t);
             }
-
         }
 
-        return new ArrayList<>(mp.values());
+        for(List<String> st : mp.values()){
+            answer.add(st);
+        }
+        return answer;
     }
 }
