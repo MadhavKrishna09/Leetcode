52eb80444ace2592bba825ce704dd35aa796ebfb
Madhav Krishna <103254884+MadhavKrishna09@users.noreply.github.com>
2025-07-11 18:28:46 +0530

    Time: 8 ms (13.79%) | Memory: 49.5 MB (71.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,34 +1,41 @@
 class Solution {
-    public void dfs(char[][] grid, int i, int j, int[][] visited, int r, int c){
-        if(grid[i][j] == '0' ) return;
-        // visited[i][j] = 1;
-        if(grid[i][j] == '1' && visited[i][j] == 0) {
-            visited[i][j] =1;
-            // ans++;
-            if(i-1>=0) dfs(grid, i-1, j, visited, r, c);
-            if(i+1<r)  dfs(grid, i+1, j, visited, r, c);
-            if(j+1<c)  dfs(grid, i, j+1, visited, r, c);
-            if(j-1>=0) dfs(grid, i, j-1, visited, r, c);
-         }
-        else return;
-        
-    }
-
+    int[][] dir = {{0,1}, {0,-1}, {1, 0}, {-1,0}};
     public int numIslands(char[][] grid) {
         int r = grid.length;
         int c = grid[0].length;
         int[][] visited = new int[r][c];
-        int ans = 0;
+        int answer = 0;
 
-        for(int i=0;i<r;i++){
+        for(int i = 0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(grid[i][j] != '0'  && visited[i][j] == 0) {
-                    ans++;
-                    dfs(grid, i, j, visited, r, c);
+                if(grid[i][j] == '1' && visited[i][j] ==0){
+                    answer++;
+                    dfs(grid, i, j, r, c, visited);
                 }
             }
         }
+        return answer;
 
-        return ans;
+
+    }
+
+    private void dfs(char[][] grid, int i, int j, int r, int c, int[][] visited){
+        visited[i][j] = 1;
+        Stack<int[]> st = new Stack<>();
+        st.push(new int[]{i, j});
+        while(!st.isEmpty()){
+         int[] x = st.pop();
+
+        for(int[] d : dir){
+            int nr = x[0]+d[0];
+            int nc = x[1]+d[1];
+
+            if(nr >=0 && nr < r && nc>=0 && nc<c && visited[nr][nc] ==0 && grid[nr][nc] == '1'){
+                visited[nr][nc] = 1;
+                st.push(new int[]{nr, nc});
+
+            }
+        }
+        }
     }
 }
